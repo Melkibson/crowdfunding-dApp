912baac5c41c4db3794cde522c6cbffb89f79cd6
Melkibson <melkiyamna@protonmail.com>
2023-03-23 17:00:13 +0100

    Deploy CrowdFunding smart contract

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -13,4 +13,55 @@ contract CrowdFunding {
         address[] donators;
         uint256[] donations;
     }
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public nbOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256){
+        Campaign storage campaign = campaigns[nbOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline has not been reached");
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        nbOfCampaigns++;
+
+        return nbOfCampaigns - 1;
+
+    }
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](nbOfCampaigns);
+
+        for(uint i = 0; i < nbOfCampaigns; i++){
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
 }
