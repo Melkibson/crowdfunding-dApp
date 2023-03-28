de7e2dfd73461e8ab78e1e3815ae895ca164ac20
Melkibson <melkiyamna@protonmail.com>
2023-03-28 22:22:58 +0200

    Remove getCampaigns method to iterate campaigns on the client side to save gas

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -15,6 +15,7 @@ contract CrowdFunding {
     }
     mapping(uint256 => Campaign) public campaigns;
     uint256 public nbOfCampaigns = 0;
+    event Donation(uint256 indexed campaignId, address indexed donator, uint256 amount);
 
     function createCampaign(
         address _owner,
@@ -24,7 +25,7 @@ contract CrowdFunding {
         uint256 _deadline,
         string memory _image
     ) public returns (uint256){
-        Campaign storage campaign = campaigns[nbOfCampaigns];
+        Campaign storage campaign = campaigns[nbOfCampaigns++];
 
         require(campaign.deadline < block.timestamp, "The deadline has not been reached");
         campaign.owner = _owner;
@@ -35,33 +36,22 @@ contract CrowdFunding {
         campaign.amountCollected = 0;
         campaign.image = _image;
 
-        nbOfCampaigns++;
-
         return nbOfCampaigns - 1;
 
     }
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
-        Campaign storage campaign = campaigns[_id];
-        campaign.donators.push(msg.sender);
-        campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value: amount}("");
-        if(sent){
-            campaign.amountCollected = campaign.amountCollected + amount;
-        }
+        Campaign storage campaign = campaigns[_id];
+        emit Donation(_id, msg.sender, amount);
+
+        payable(campaign.owner).transfer(amount);
+        campaign.amountCollected += amount;
+
     }
+
     function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
-    }
-    function getCampaigns() public view returns (Campaign[] memory) {
-        Campaign[] memory allCampaigns = new Campaign[](nbOfCampaigns);
-
-        for(uint i = 0; i < nbOfCampaigns; i++){
-            Campaign storage item = campaigns[i];
-            allCampaigns[i] = item;
-        }
-        return allCampaigns;
     }
 }
